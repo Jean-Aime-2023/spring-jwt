2ba5760d23071d7637b3c5c9d8418e3dccac479f
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-06-16 21:57:56 +0200

    updates

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -1,10 +1,9 @@
-import * as React from 'react'
-export default function Buttons(props){
-    return(
-        <div>
-            <button onClick={props.login}>Login</button>
-            <button onClick={props.logout}>Logout</button>
-        </div>
-    )
-    
+import * as React from 'react';
+export default function Buttons(props) {
+  return (
+    <div>
+      <button onClick={props.login}>Login</button>
+      <button onClick={props.logout}>Logout</button>
+    </div>
+  );
 }
